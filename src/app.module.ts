0b0e5c4639bfbd5350a2a9b1938f26eecf0f5391
Jonathan Damián Navarro Vega <jonathan.d.navarro.v@gmail.com>
2025-05-11 00:36:52 -0400

    chore: Instalación de cross-env y configuración de variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import config from "config/env-config";
 
 @Module({
-	imports: [],
+	imports: [
+		// Se configuran las variables de entorno
+		ConfigModule.forRoot({
+			envFilePath: `./env/${process.env.NODE_ENV}.env`,
+			load: [config],
+			isGlobal: true, // Aplica la configuración a toda la aplicación
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
