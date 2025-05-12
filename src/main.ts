685036e110f6c552f213a9abfae8d997d0f03e98
Jonathan Navarro <jonathan.d.navarro.v@gmail.com>
2025-05-11 21:01:03 -0400

    chore: Configuración de GlobalPipes para validaciones entrantes de datos

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(new ValidationPipe()); // Aplica validaciones a las peticiones entrantes para validar datos a nivel global
 	app.useGlobalInterceptors(new ResponseInterceptor(app.get(Reflector))); // Se inyecta el reflector en el interceptor
 
 	await app.listen(process.env.API_PORT ?? 3000);
