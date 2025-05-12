8927c019c10711437772b1635814ced30897c8db
Jonathan Navarro <jonathan.d.navarro.v@gmail.com>
2025-05-12 16:28:37 -0400

    refactor: Se mueve la carpeta de interceptors a su ubicación correcta

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory, Reflector } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { ResponseInterceptor } from "interceptors/response.interceptor";
+import { ResponseInterceptor } from "src/interceptors/response.interceptor";
 import { ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
