d4140430e401f44bbecf381b802fe3523268b4ba
Jonathan Navarro <jonathan.d.navarro.v@gmail.com>
2025-06-07 19:17:20 -0400

    feat(branches): Creación del módulo, controller y service de branches

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import config from "config/env-config";
 import { AuthModule } from "./modules/auth/auth.module";
 import { UsersModule } from "./modules/users/users.module";
+import { BranchesModule } from "./modules/branches/branches.module";
 
 @Module({
 	imports: [
@@ -31,6 +32,7 @@ import { UsersModule } from "./modules/users/users.module";
 		}),
 		UsersModule,
 		AuthModule,
+		BranchesModule,
 	],
 })
 export class AppModule {}
